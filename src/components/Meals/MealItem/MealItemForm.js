518b6b4c2cfa8aa1d72de8029979900d8422ed58
Sanyam Atreya <samatreya@gmail.com>
2022-09-22 23:58:33 +0530

    Created Cart.jsx

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,17 +1,18 @@
 import Input from "../../UI/Input";
 import classes from './MealItemForm.module.css';
+const abc ={
+    id: 'amount_' + props.id,
+    text: 'number',
+    min: '1',
+    max: '5',
+    step: '1',
+    defaultValue: '1'
+}
 
 const MealItemForm = (props) => {
     return (
         <form className={classes.form}>
-            <Input label="Amount" input={{
-                id: 'amount_' + props.id,
-                text: 'number',
-                min: '1',
-                max: '5',
-                step: '1',
-                defaultValue: '1'
-            }} />
+            <Input label="Amount" input={abc} />
             <button>+ Add</button>
         </form>
     );
