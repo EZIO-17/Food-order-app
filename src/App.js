31d473359585f64081a1c0fafd824a3162b7bb5d
Sanyam Atreya <samatreya@gmail.com>
2022-09-26 23:43:40 +0530

    Added Modal component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Fragment } from 'react';
 import './App.css';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import Card from './components/UI/Card';
 
 function App() {
   return (
     <Fragment>
+      <Card />
       <Header />
       <main>
         <Meals />
