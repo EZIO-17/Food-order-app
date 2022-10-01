ebc1b4f63113717e4081cb5ffcb1a2374ee1fb78
EZIO-17 <samatreya@gmail.com>
2022-10-02 03:01:07 +0530

    Making items removable

--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -9,12 +9,58 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === 'ADD') {
-    const updatedItems = state.items.concat(action.item);
+    // const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+    
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    let updatedItems;
+
+    if(existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updateItems = [...state.items];
+      updateItems[existingCartItemIndex] = updatedItem;
+    
+    } else {
+      updatedItems = state.items.concat(action.item);
+    } 
+
     return {
       items: updatedItems,
-      totalAmount: updatedTotalAmount
+      totalAmount: updatedTotalAmount,
+    }
+    
+    
+  }
+  
+  if (action.type === 'REMOVE') {
+    const existingCartItemIndex =state.items.findIndex(
+      (item) => item.id === action.id
+    );
+
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.amount - existingItem.price;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter(item => item.id !== action.id);
+    } else {
+      const updatedItems = {...existingItem, amount: existingItem.amount - 1};
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
+    
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
     };
+
   }
   return defaultCartState;
 };
